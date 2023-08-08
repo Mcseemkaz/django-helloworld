a0ae992531aecd0bcf1ebc90b4d6b5a26c24cea3
ovsuk.maksim@gmail.com <ovsuk.maksim@gmail.com>
2023-08-08 15:46:46 +0300

    Modify current time function

--- helloworld/views.py
+++ helloworld/views.py
@@ -4,5 +4,9 @@ from datetime import datetime
 now = datetime.now()
 
 
+def get_current_time():
+    return now.strftime("%H:%M:%S")
+
+
 def index(request):
-    return HttpResponse("Hello, world! UPD SEVEN  Its time " + now.strftime("%H:%M:%S"))
+    return HttpResponse("Hello, world! UPD SEVEN  Its time " + get_current_time())
