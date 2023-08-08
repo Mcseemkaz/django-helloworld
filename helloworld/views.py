9f34aacac60312b0ad604ac8fdf04732eb430254
ovsuk.maksim@gmail.com <ovsuk.maksim@gmail.com>
2023-08-08 14:19:29 +0300

    Add current time

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from datetime import datetime
+
+now = datetime.now()
+
 
 def index(request):
-    return HttpResponse("Hello, world! UPD SEVEN")
+    return HttpResponse("Hello, world! UPD SEVEN  Its time " + now.strftime("%H:%M:%S"))
