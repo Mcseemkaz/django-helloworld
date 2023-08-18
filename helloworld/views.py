0f7807d2d1f15d79f8c1753689203fef740e9ad7
ovsuk.maksim@gmail.com <ovsuk.maksim@gmail.com>
2023-08-18 11:53:39 +0300

    Add developer branch and deveses

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-now = datetime.now()
-
 
 def get_current_time():
-    return now.strftime("%H:%M:%S")
+    return datetime.now().strftime("%H:%M:%S")
 
 
 def index(request):
-    return HttpResponse("Hello, world! UPD SEVEN  Its time " + get_current_time())
+    return HttpResponse("Hello, world! UPD DEVELOPES  Its time " + get_current_time())
